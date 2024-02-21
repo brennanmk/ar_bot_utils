c873b7e8638114f0f1c3fe928035f48e7a6e36ea
Brennan <brennanmk2200@gmail.com>
2024-02-20 22:39:32 -0500

    progress

--- ar_bot_simulated_testing/src/rllib/train.py
+++ ar_bot_simulated_testing/src/rllib/train.py
@@ -2,45 +2,38 @@ from ray.rllib.algorithms.ppo import PPOConfig
 from ar_bot_sim.environments.tabletop.ar_bot_tabletop_gym import ARBotTabletopGym
 import gymnasium as gym
 from ray import tune, air, train
+
 actions = gym.spaces.Discrete(4)
 
-action_mapping = {
-    0: (0.0, 0.5),
-    1: (0.5, 0.0),
-    2: (0.0, -0.5),
-    3: (-0.5, 0.0)
-}
+action_mapping = {0: (0.0, 0.5), 1: (0.5, 0.0), 2: (0.0, -0.5), 3: (-0.5, 0.0)}
 
 # Create an RLlib Algorithm instance from a PPOConfig object.
 config = (
-    PPOConfig().environment(
+    PPOConfig()
+    .environment(
         env=ARBotTabletopGym,
         env_config={
             "discrete_action_mapping": action_mapping,
             "render_simulation": False,
             "number_of_obstacles": 0,
             "actions": actions,
-            "max_timesteps_per_episode": 4000
+            "max_timesteps_per_episode": 4000,
         },
     )
     # Parallelize environment rollouts.
-    .rollouts(num_rollout_workers=12)
+    .rollouts(num_rollout_workers=1)
     .resources(num_gpus=1)
-    .evaluation(
-        evaluation_num_workers=1,
-        evaluation_interval=20,
-    )
+    .training(gamma=0.99, lr=1e-5, clip_param=0.2, model={"fcnet_hiddens": [64, 64]})
 )
 
 # Train for n iterations and report results (mean episode rewards).
 
-stopping_criteria = {"episode_reward_mean": 700}
-tune.Tuner("PPO", run_config=air.RunConfig(stop=stopping_criteria, checkpoint_config=train.CheckpointConfig(checkpoint_at_end=True)), param_space=config.to_dict()).fit()
-
-save_result = aglo.save()
-
-path_to_checkpoint = save_result.checkpoint.path
-print(
-    "An Algorithm checkpoint has been created inside directory: "
-    f"'{path_to_checkpoint}'."
-)
+stopping_criteria = {"episode_reward_mean": 100}
+tune.Tuner(
+    "PPO",
+    run_config=air.RunConfig(
+        stop=stopping_criteria,
+        checkpoint_config=train.CheckpointConfig(checkpoint_at_end=True),
+    ),
+    param_space=config.to_dict(),
+).fit()
