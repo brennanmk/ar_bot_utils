c6bc25acd25eda9cd11dff8c9d1227fc401db6fa
Brennan <you@example.com>
2024-03-07 21:17:35 -0500

    progress

--- ar_bot_simulated_testing/src/rllib/train.py
+++ ar_bot_simulated_testing/src/rllib/train.py
@@ -5,6 +5,7 @@ from ray import tune, air, train
 import requests
 import numpy as np
 import argparse
+import os
 
 class TrainARBot:
     def __init__(self, args) -> None:
@@ -23,7 +24,6 @@ class TrainARBot:
             PPOConfig()
             .environment(
                 env=ARBotTabletopGym,
-                env_task_fn=self.curriculum_fn,
                 env_config={
                     "discrete_action_mapping": action_mapping,
                     "render_simulation": False,
@@ -35,33 +35,29 @@ class TrainARBot:
             .framework("torch")
             .rollouts(num_rollout_workers=args.workers)
             .resources(num_gpus=args.gpu)
-            .training(gamma=args.discount, lr=args.lr, clip_param=args.clip_param, model={"fcnet_hiddens": args.network})
+            .training(gamma=args.discount, lr=args.lr, clip_param=args.clip_param, model={"fcnet_hiddens": args.network}, train_batch_size=args.batch_size)
             .evaluation(evaluation_interval=self.evaluation_interval, evaluation_duration=args.evaluation_duration, evaluation_config={"expore":False})
         )
 
-        for expirement in args.num_expirements:
-            results = tune.Tuner(
+        for expirement in range(args.num_expirements):
+            trial_name = f"{args.expirement_name}_{expirement}"
+            tune.Tuner(
                 "PPO",
-                run_config=train.RunConfig(stop=self.eval_callback),
+                run_config=train.RunConfig(stop=self.eval_callback, name = trial_name, local_dir=os.path.join(os.getcwd(), "results")),
                 param_space=config.to_dict(),
             ).fit()
 
-            results.get_dataframe().to_csv(f"{args.expirement_name}_{expirement}.csv")
+            requests.post("https://ntfy.bmillerklugman.me/phone",
+                data=f"{trial_name} complete".encode(encoding='utf-8'))
+
+        requests.post("https://ntfy.bmillerklugman.me/phone",
+            data=f"Training {expirement} complete".encode(encoding='utf-8'))
 
     def eval_callback(self, trial_id: str, result: dict) -> bool:
         if "evaluation" in result:
             evaluation_mean_reward = result['evaluation']['sampler_results']['episode_reward_mean']
             return evaluation_mean_reward >= self.stop_reward or result["training_iteration"] >= self.max_iterations
         return result["training_iteration"] >= self.max_iterations
-
-    def curriculum_fn(self, train_results, task_settable_env, env_ctx):
-        task = task_settable_env.get_task()
-
-        evaluation_mean_reward = train_results['evaluation']['sampler_results']['episode_reward_mean']
-        if evaluation_mean_reward >= self.stop_reward and self.cirriculum and task < self.obstacles:
-            task += 1
-
-        return task
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='ARBot rllib testing')
@@ -80,6 +76,7 @@ if __name__ == '__main__':
     parser.add_argument('--iterations', type=int, default=20000, help='number of iterations total to use during training')
     parser.add_argument('--num_expirements', type=int, default=1, help='number of expirements to collect')
     parser.add_argument('--expirement_name', type=str, default=None, help='name of expirement to save csv to')
+    parser.add_argument('--batch_size', type=int, default=4000, help='size of train batch')
 
     parser.add_argument('--back', help='If true, bot can go back', default=False)
     parser.add_argument('--cirriculum', help='If true, train with cirr', default=False)
--- ar_bot_simulated_testing/src/rllib/train.sh
+++ ar_bot_simulated_testing/src/rllib/train.sh
@@ -1,4 +1,5 @@
 # #!/bin/sh
-python3 train.py --workers 14 --gpu 1 --timesteps 400 --lr 1e-3 --stop_return -3.0 --obstacles 0 --num_expirements 3 --expirement_name "basic"
-python3 train.py --workers 14 --gpu 1 --timesteps 400 --lr 1e-3 --stop_return -3.0 --obstacles 5 --cirriculum True --num_expirements 3 --save_location "cirr"
-python3 train.py --workers 14 --gpu 1 --timesteps 400 --lr 1e-3 --stop_return -3.0 --obstacles 5 --num_expirements 3 --expirement_name "full"
+python3 train.py --workers 4 --gpu 1 --timesteps 400 --lr 1e-3 --stop_return -3.0 --obstacles 0 --num_expirements 3 --expirement_name "basic-4"
+python3 train.py --workers 6 --gpu 1 --timesteps 400 --lr 1e-3 --stop_return -3.0 --obstacles 0 --num_expirements 3 --expirement_name "basic-6"
+python3 train.py --workers 8 --gpu 1 --timesteps 400 --lr 1e-3 --stop_return -3.0 --obstacles 0 --num_expirements 3 --expirement_name "basic-8"
+python3 train.py --workers 14 --gpu 1 --timesteps 400 --lr 1e-3 --stop_return -3.0 --obstacles 0 --num_expirements 3 --expirement_name "basic-large-batch" --batch_size=16000
